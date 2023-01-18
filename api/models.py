72d1c3733521a1065ff80839d9903f4b3d53b341
Tomoya Bansho <gku14580@kwansei.ac.jp>
2023-01-19 00:51:45 +0900

    UserModelの追加

---
+++ api/models.py
@@ -0,0 +1,69 @@
+from django.db import models
+from django.core.mail import send_mail
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.utils import timezone
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+
+        if not email:
+            raise ValueError('メールアドレスは設定されている必要があります')
+        email = self.normalize_email(email)
+
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('スーパユーザは必ずis_staffをTrueにする必要があります')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('スーパユーザは必ずis_superuserをTrueにする必要があります')
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField('email address', unique=True)
+
+    is_staff = models.BooleanField(
+        'staff status',
+        default=False,
+        help_text='admin siteにアクセスできるかどうかを設定します',
+    )
+    is_active = models.BooleanField(
+        'active',
+        default=True,
+        help_text='ユーザアカウントが使用可能かどうかを設定します',
+    )
+    date_joined = models.DateTimeField('date joined', default=timezone.now)
+
+    objects = UserManager()
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+
+    def send_mail(self, subject, message, from_email=None, **kwargs):
+        """Send an email to this user."""
+        send_mail(subject, message, from_email, [self.email], **kwargs)
