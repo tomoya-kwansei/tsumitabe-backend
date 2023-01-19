c52523f8e304c6aef6dfe5725e9fa768eb2004b6
Tomoya Bansho <gku14580@kwansei.ac.jp>
2023-01-19 16:18:27 +0900

    レスポンスのユーザ情報を制限

--- api/serializers.py
+++ api/serializers.py
@@ -5,4 +5,4 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('__all__')
+        fields = ('id', 'email', 'date_joined')
