6a5fe2b2e3f664c489670fc347f4d09f9cd2d698
Tomoya Bansho <gku14580@kwansei.ac.jp>
2023-01-28 20:28:01 +0900

    change meta text

--- api/middlewares.py
+++ api/middlewares.py
@@ -6,7 +6,7 @@ class TokenAuthenticateMiddleware:
 
     def __call__(self, request):
         # ここに処理を記述すると、view関数が実行される前に実行される
-        header_text = request.META.get("Authorization", None)
+        header_text = request.META.get("HTTP_AUTHORIZATION", None)
         if header_text != None:
             token = header_text.split(" ")[1]
             request.user = Token.objects.get(key=token).user
